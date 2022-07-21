da2e2e42885783eb233ba3360deb64340a839de2
Hikmat Hidayat <hikmat6@gmail.com>
2022-07-21 15:33:06 +0700

    second commit

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class LandingPages extends Component{
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
