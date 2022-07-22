b128661bc62838fc31a20969c133ee7501616078
Hikmat Hidayat <hikmat6@gmail.com>
2022-07-22 14:03:44 +0700

    22Juli2022

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import 'assets/scss/style.scss';
 import LandingPages from 'pages/LandingPage';
 import { Routes, Route } from 'react-router-dom';
+
 function App() {
   return (  
-      <Routes>
-        <Route path="/" element={LandingPages} />
-      </Routes>     
+    <Routes>
+      <Route path="/" element={<LandingPages />} />
+    </Routes>    
   );
 }
 
